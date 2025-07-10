5af38abd37372d60de4e84a375b253a1fd1197a4
Edwin Saa <edwin.saa@udla.edu.ec>
2025-07-10 08:00:16 -0500

    Creation of to scripts in the folder services, one for the log service (LogService) and the other for the sqlite database (RecipeDatabase)

---
+++ EdwinSaaEvaluacionP3/EdwinSaaEvaluacionP3/Services/LogService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdwinSaaEvaluacionP3.Services
+{
+    public class LogService
+    {
+
+        public static async Task AppendLogAsync(string recipeName)
+        {
+            string logFileName = Path.Combine(FileSystem.AppDataDirectory, "Logs_Saa.txt");//para la crecion y registrio de los logs
+            string logEntry = $"Se incluyó el registro {recipeName} el {DateTime.Now:dd/MM/yyyy HH:mm}\n";
+            await File.AppendAllTextAsync(logFileName, logEntry);
+        }
+
+        public static async Task<string> ReadLogsAsync()
+        {
+            string logFileName = Path.Combine(FileSystem.AppDataDirectory, "Logs_Saa.txt");// para la lectura de los logs
+            return File.Exists(logFileName) ? await File.ReadAllTextAsync(logFileName) : "No logs found.";
+        }
+
+    }
+}
--- EdwinSaaEvaluacionP3/EdwinSaaEvaluacionP3/Services/RecipeDatabase.cs
+++ EdwinSaaEvaluacionP3/EdwinSaaEvaluacionP3/Services/RecipeDatabase.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
+using EdwinSaaEvaluacionP3.Models;
 
 namespace EdwinSaaEvaluacionP3.Services
 {
-    internal class RecipeDatabase
+    public class RecipeDatabase
     {
+
+        private readonly SQLiteAsyncConnection _database;
+
+        public RecipeDatabase(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Recipe>().Wait();
+        }
+
+        public Task<List<Recipe>> GetRecipesAsync() => _database.Table<Recipe>().ToListAsync();
+
+        public Task<int> SaveRecipeAsync(Recipe recipe) => _database.InsertAsync(recipe);
+
+
     }
 }
