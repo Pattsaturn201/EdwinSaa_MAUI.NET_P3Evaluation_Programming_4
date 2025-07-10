909a536ec53b2ee6585906009b488ff603e10164
Edwin Saa <edwin.saa@udla.edu.ec>
2025-07-10 07:47:23 -0500

    Creacion of folder estructure for MVVM and creation of Recipe model

---
+++ EdwinSaaEvaluacionP3/EdwinSaaEvaluacionP3/Models/Recipe.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace EdwinSaaEvaluacionP3.Models
+{
+    public class  Recipe 
+    {
+        [PrimaryKey]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Ingredients { get; set; }
+        public int PreparationTimeMinutes { get; set; }
+
+        public bool IsVegetarian { get; set; }
+
+    }
+
+}
---
+++ EdwinSaaEvaluacionP3/EdwinSaaEvaluacionP3/Services/RecipeDatabase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdwinSaaEvaluacionP3.Services
+{
+    internal class RecipeDatabase
+    {
+    }
+}
