bd09347b4654e6ef51bf23b31615d8ef24b8f12c
Edwin Saa <edwin.saa@udla.edu.ec>
2025-07-10 07:19:09 -0500

    Add project files.

---
+++ EdwinSaaEvaluacionP3/EdwinSaaEvaluacionP3/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace EdwinSaaEvaluacionP3
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
